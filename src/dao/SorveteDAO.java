f603fa242f23a8fbc743e25b42995ea02b9daba6
JeffeDev <jeffe.info@gmail.com>
2021-10-18 19:52:53 -0300

    :sparkles: SorveteDAO Método insertSorvete

--- src/dao/SorveteDAO.java
+++ src/dao/SorveteDAO.java
@@ -1,5 +1,17 @@
 package dao;
 
-public class SorveteDAO {
+import domain.Sorvete;
+import litebase.PreparedStatement;
+import totalcross.sql.SQLException;
+import util.DataBaseManager;
 
+public class SorveteDAO {
+	public void insertSorvete(Sorvete sorvete) throws SQLException, java.sql.SQLException{
+		PreparedStatement ps = (PreparedStatement) DataBaseManager.getConnection().prepareStatement("INSERT INTO SORVETES VALUES(?,?,?)");
+		ps.setString(1, sorvete.sabor);
+		ps.setDouble(2, sorvete.valor);
+		ps.setDouble(3, sorvete.estoque);
+	}
 }
+		
+		
