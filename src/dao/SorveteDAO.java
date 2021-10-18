ac918a1fa8c9999a63761ea96ff0f0dbc3a39912
JeffeDev <jeffe.info@gmail.com>
2021-10-18 20:23:42 -0300

    :ok_handle: método executeUpdate SorveteDAO

--- src/dao/SorveteDAO.java
+++ src/dao/SorveteDAO.java
@@ -6,11 +6,15 @@ import totalcross.sql.SQLException;
 import util.DataBaseManager;
 
 public class SorveteDAO {
-	public void insertSorvete(Sorvete sorvete) throws SQLException, java.sql.SQLException{
+	public boolean insertSorvete(Sorvete sorvete) throws SQLException, java.sql.SQLException{
 		PreparedStatement ps = (PreparedStatement) DataBaseManager.getConnection().prepareStatement("INSERT INTO SORVETES VALUES(?,?,?)");
 		ps.setString(1, sorvete.sabor);
 		ps.setDouble(2, sorvete.valor);
 		ps.setDouble(3, sorvete.estoque);
+		
+		int inserted = ps.executeUpdate();
+		ps.close();
+		return inserted > 0 ? true : false;
 	}
 }
 		
--- src/domain/Sorvete.java
+++ src/domain/Sorvete.java
@@ -4,4 +4,29 @@ public class Sorvete {
 	public String sabor;
 	public double valor;
 	public double estoque;
+	
+	public Sorvete() {
+		super();
+	}
+	
+	public String getSabor() {
+		return sabor;
+	}
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public double getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(double estoque) {
+		this.estoque = estoque;
+	}
 }
