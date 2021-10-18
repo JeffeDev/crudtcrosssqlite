0cfa98f5156b12ca656ad6509d88646aaf685971
JeffeDev <jeffe.info@gmail.com>
2021-10-18 18:23:52 -0300

    :faet: método para criar o banco SORVETES as tabelas do banco

--- src/util/DataBaseManager.java
+++ src/util/DataBaseManager.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import totalcross.db.sqlite.SQLiteUtil;
 import totalcross.sql.Connection;
+import totalcross.sql.Statement;
 import totalcross.sys.Settings;
 import totalcross.sys.Vm;
 
@@ -18,9 +19,14 @@ public class DataBaseManager {
 		}
 	}
 	
-	
 	public static Connection getConnection() throws SQLException{
 		return sqLiteUtil.con();
+	}
+	
+	public static void loadTabelas() throws SQLException {
+		Statement st = getConnection().createStatement();
+		st.execute("CREATE TABLE IF NOT EXISTS  SORVETES (SABOR VARCHAR, VALOR NUMERIC, ESTOQUE NUMERIC)");
+		st.close();
 	}
 	
 }
